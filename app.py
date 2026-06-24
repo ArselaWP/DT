926e58279ca008f3bff8bd856299172d9d02e7f4
Arsela <mustikaningrum@student.telkomuniversity.ac.id>
2026-06-24 15:15:41 +0700

    Update .gitignore and add outputs folder

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-from dashboard.app import main
+import pandas as pd
+import os
 
+# Gunakan path relatif
+data_path = os.path.join(os.path.dirname(__file__), 'outputs', 'food_consumption_clustered_for_dashboard.csv')
 
-if __name__ == "__main__":
-    main()
+# Cek apakah file ada sebelum dibaca
+if os.path.exists(data_path):
+    df = pd.read_csv(data_path)
+else:
+    st.error(f"File tidak ditemukan di: {data_path}")
+    # Opsi lain: tampilkan pesan error yang lebih informatif
